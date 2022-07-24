d2a3683ff9c50f5673ace1e36a3c3946a9d189fc
Tarek AZAIZ <tarek.azaiz@live.fr>
2022-07-24 18:41:30 -0400

    renamed files and made them classes

---
+++ SpoofedSignalTransmitter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class SpoofedSignalTransmitter():
+
+    def transmit():
+        """
+        """
+        return 0
+
+def main():
+    """
+    """
+    SpoofedSignalTransmitter.transmit()
+    return 0
+
+if __name__ == "__main__":
+    main()
