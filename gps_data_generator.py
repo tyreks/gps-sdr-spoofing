e62460e85aeb993adca644dc2f2d4ba70ec0271b
Tarek AZAIZ <tarek.azaiz@live.fr>
2022-07-30 21:32:20 +0200

    generator now handles parameters from the config file

--- gps_data_generator.py
+++ gps_data_generator.py
@@ -2,39 +2,50 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+from configparser import ConfigParser
 import datetime
 import subprocess
 
 
-GPS_SDR_SIM = "../gps-sdr-sim/gps-sdr-sim"
-INPUT_EPHEM_FILE="gps_data.n"
-SAMPLE_FREQ="2600000"
-DATE=datetime.datetime.today().strftime("%Y/%m/%d,07:%M:%S")
-BITS="8"
-LOCATION="35.504452495375716,11.057292641491118,100"
-DURATION = "30"
+#LOCATION="35.504452495375716,11.057292641491118,100"
 
 class GpsDataGenerator(object):
 
-    def __init__(self, date=DATE, input_ephem_file=INPUT_EPHEM_FILE
-        ,bits=BITS, sample_freq=SAMPLE_FREQ, location=LOCATION, duration=DURATION):
+    def __init__(self, date=None, input_ephem_file=None
+        ,bits=None, sample_rate=None, location=None, duration=None):
         """
         """
-        self.date = date
-        self.input_ephem_file = input_ephem_file
-        self.bits = bits
-        self.sample_freq = sample_freq
+        parser = ConfigParser()
+        parser.read("config_gps_spoofer.ini")
+
+        self.input_ephem_file = input_ephem_file \
+            if input_ephem_file != None \
+            else parser["GENERATOR"]["INPUT_EPHEM_FILE"]
+
+        self.bits = bits if bits != None \
+            else parser["GENERATOR"]["BITS"]
+
+        self.sample_rate = sample_rate if sample_rate != None \
+            else parser["GENERATOR"]["SAMPLE_RATE"]
+            
+        self.duration = duration if duration != None \
+            else parser["GENERATOR"]["DURATION"]
+
+        self.date = date if date != None\
+            else datetime.datetime.today().strftime("%Y/%m/%d,07:%M:%S")
+
         self.location = location
-        self.duration = duration
+
+        self.gps_sdr_sim = parser["GENERATOR"]["GPS_SDR_SIM"]
 
 
     def generate_gps_data(self):
         """
         """
         try:
-            subprocess.run([GPS_SDR_SIM, "-t", self.date, "-e"
+            subprocess.run([self.gps_sdr_sim, "-t", self.date, "-e"
                 , self.input_ephem_file, "-b", self.bits, "-s"
-                , self.sample_freq, "-l", self.location, "-d"
+                , self.sample_rate, "-l", self.location, "-d"
                 , self.duration], capture_output=False)
         except Exception as e:
             print("\nError during GPS data generation :\n"+format(e)+"\n\n")
@@ -48,20 +59,24 @@ def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         add_help=True, description="Generate the associated"\
             " binary from the local GNSS data file.")
-    
-    parser.add_argument("-t", dest="date", default=DATE
-        , help="date (YYYY/MM/DD,HH:MM:SS)")
+
+    parser.add_argument("-l", dest="location", required=True
+        , help="GPS location to transmit.")
+
+    parser.add_argument("-t", dest="date"
+        , help="date (YYYY/MM/DD,HH:MM:SS).")
+
     parser.add_argument("-e", dest="input_ephem_file"
-        , default=INPUT_EPHEM_FILE
-        , help="GNSS ephemeris file")
-    parser.add_argument("-b", dest="bits", default=BITS
-        , help="Number of bits")
-    parser.add_argument("-s", dest="sample_freq", default=SAMPLE_FREQ
-        , help="Sample frequency (Hz)")
-    parser.add_argument("-l", dest="location", default=LOCATION
-        , help="Location")
-    parser.add_argument("-d", dest="duration", default=DURATION
-        , help="Duration (s)")
+        , help="GNSS ephemeris file.")
+
+    parser.add_argument("-b", dest="bits"
+        , help="Number of bits.")
+
+    parser.add_argument("-s", dest="sample_rate"
+        , help="Sample rate (Hz).")
+
+    parser.add_argument("-d", dest="duration"
+        , help="Duration of the signal transmission.")
 
     return parser
 
@@ -73,7 +88,7 @@ def main():
 
     my_generator = GpsDataGenerator(date=args.date
         , input_ephem_file=args.input_ephem_file, bits=args.bits
-        , sample_freq=args.sample_freq, location=args.location
+        , sample_rate=args.sample_rate, location=args.location
         , duration=args.duration)
 
     my_generator.generate_gps_data()
